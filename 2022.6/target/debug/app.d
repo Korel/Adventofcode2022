bd8d98a9557d5671780585eb7ae49d5f6dac4272
Korel <korelerben@gmail.com>
2023-10-01 20:47:34 +0200

    2022.6

---
+++ 2022.6/target/debug/app.d
@@ -0,0 +1,1 @@
+/home/korel/projects/adventofcode/2022.6/target/debug/app: /home/korel/projects/adventofcode/2022.6/src/main.rs
---
+++ 2022.6/target/debug/deps/app-82c2b219b623fa76.d
@@ -0,0 +1,5 @@
+/home/korel/projects/adventofcode/2022.6/target/debug/deps/app-82c2b219b623fa76: src/main.rs
+
+/home/korel/projects/adventofcode/2022.6/target/debug/deps/app-82c2b219b623fa76.d: src/main.rs
+
+src/main.rs:
