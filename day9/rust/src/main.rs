ad382690465ac63f0868d541ddf954af8a3dd975
Korel <korelerben@gmail.com>
2023-10-22 23:35:42 +0200

    Added Rust implementation of Python solution without draw

--- day9/rust/src/main.rs
+++ day9/rust/src/main.rs
@@ -1,60 +1,98 @@
 use std::collections::HashSet;
 use std::fs;
 
+fn distance(node0: (i32, i32), node1: (i32, i32)) -> f32 {
+    f32::sqrt((node0.0 - node1.0).pow(2) as f32 + (node0.1 - node1.1).pow(2) as f32)
+}
+
+fn move_other_nodes(nodes: &mut [(i32, i32)]) {
+    for i in 0..nodes.len() - 1 {
+        let x0 = nodes[i].0;
+        let y0 = nodes[i].1;
+        let mut x1 = nodes[i + 1].0;
+        let mut y1 = nodes[i + 1].1;
+        if distance(nodes[i], nodes[i + 1]) >= 2.0 {
+            if x0 == x1 {
+                if y0 > y1 {
+                    y1 += 1;
+                } else {
+                    y1 -= 1;
+                }
+            } else if y0 == y1 {
+                if x0 > x1 {
+                    x1 += 1;
+                } else {
+                    x1 -= 1;
+                }
+            } else {
+                if y0 > y1 {
+                    y1 += 1;
+                } else {
+                    y1 -= 1;
+                }
+                if x0 > x1 {
+                    x1 += 1;
+                } else {
+                    x1 -= 1;
+                }
+            }
+        }
+        nodes[i + 1].0 = x1;
+        nodes[i + 1].1 = y1;
+    }
+}
+
+fn move_head(nodes: &mut [(i32, i32)], direction: char) {
+    let head: &mut (i32, i32) = nodes.get_mut(0).unwrap();
+    match direction {
+        'U' => head.1 += 1,
+        'D' => head.1 -= 1,
+        'L' => head.0 -= 1,
+        'R' => head.0 += 1,
+        _ => {}
+    }
+    move_other_nodes(nodes);
+}
+
 fn first_task(moves: Vec<(char, i32)>) {
-    let mut head = (0, 0);
-    let mut tail = (0, 0);
+    let mut nodes = vec![(0, 0), (0, 0)];
     let mut unique_tails = HashSet::<(i32, i32)>::new();
     for mv in moves {
-        match mv.0 {
-            'U' => {
-                for _ in 0..mv.1 {
-                    head.1 += 1;
-                    let diff: i32 = head.1 - tail.1;
-                    if diff.abs() > 1 {
-                        tail.1 += 1;
-                        tail.0 = head.0;
-                        unique_tails.insert(tail);
-                    }
-                }
-            }
-            'D' => {
-                for _ in 0..mv.1 {
-                    head.1 -= 1;
-                    let diff: i32 = head.1 - tail.1;
-                    if diff.abs() > 1 {
-                        tail.1 -= 1;
-                        tail.0 = head.0;
-                        unique_tails.insert(tail);
-                    }
-                }
-            }
-            'R' => {
-                for _ in 0..mv.1 {
-                    head.0 += 1;
-                    let diff: i32 = head.0 - tail.0;
-                    if diff.abs() > 1 {
-                        tail.0 += 1;
-                        tail.1 = head.1;
-                        unique_tails.insert(tail);
-                    }
-                }
-            }
-            'L' => {
-                for _ in 0..mv.1 {
-                    head.0 -= 1;
-                    let diff: i32 = head.0 - tail.0;
-                    if diff.abs() > 1 {
-                        tail.0 -= 1;
-                        tail.1 = head.1;
-                        unique_tails.insert(tail);
-                    }
-                }
-            }
-            _ => todo!("Does not reach"),
+        let direction = mv.0;
+        let mut times = mv.1;
+        while times > 0 {
+            times -= 1;
+            move_head(nodes.as_mut_slice(), direction);
+            unique_tails.insert(*nodes.last().unwrap());
         }
     }
-    println!("{}", unique_tails.len());
+    println!("first_task: {}", unique_tails.len());
+}
+
+fn second_task(moves: Vec<(char, i32)>) {
+    let mut nodes = vec![
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+        (0, 0),
+    ];
+    let mut unique_tails = HashSet::<(i32, i32)>::new();
+    for mv in moves {
+        let direction = mv.0;
+        let mut times = mv.1;
+        while times > 0 {
+            times -= 1;
+            move_head(nodes.as_mut_slice(), direction);
+            unique_tails.insert(*nodes.last().unwrap());
+        }
+    }
+    println!("second_task: {}", unique_tails.len());
 }
 
 fn main() {
@@ -71,5 +109,6 @@ fn main() {
             split.next().unwrap().parse().unwrap(),
         ));
     }
-    first_task(moves)
+    first_task(moves.clone());
+    second_task(moves);
 }
