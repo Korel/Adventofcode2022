3f0ff1363df31f9d699618b5bb3d47f649e9269c
Korel <korelerben@gmail.com>
2023-10-22 17:32:51 +0200

    Day 9 second task is finally done in python by completely rewriting the implementation as a generic case, which solves the first task too, Rust implementation is not done yet

--- 2022.9/python/main.py
+++ 2022.9/python/main.py
@@ -1,124 +1,123 @@
-def draw(h, t, move):
-    from matplotlib import pyplot as plt
-    plt.clf()
-    axis = list(range(-51, 50))
-    plt.xlim([-50, 50])
-    plt.ylim([-50, 50])
-    plt.plot(axis, [0] * len(axis), color="cyan", label=f"{len(unique_tails)}")
-    plt.plot([0] * len(axis), axis, color="cyan", label=f"{move}")
-    plt.plot([h[0]], [h[1]], ".", color="red", label=f"({h[0]}, {h[1]})")
-    plt.plot([t[0]], [t[1]], ".", color="blue", label=f"({t[0]}, {t[1]})")
-    plt.legend()
-    plt.show(block=False)
-    plt.draw()
-    plt.pause(0.1)
+from copy import deepcopy
+from math import sqrt
 
 
-def first_task(moves):
-    head = [0, 0]
-    tail = [0, 0]
-    unique_tails = set()
-    for move in moves:
-        match move[0]:
-            case "U":
-                while move[1]:
-                    move[1] -= 1
-                    head[1] += 1
-                    if abs(head[1] - tail[1]) > 1:
-                        tail[1] += 1
-                        tail[0] = head[0]
-                        unique_tails.add(tuple(tail))
-            case "D":
-                while move[1]:
-                    move[1] -= 1
-                    head[1] -= 1
-                    if abs(head[1] - tail[1]) > 1:
-                        tail[1] -= 1
-                        tail[0] = head[0]
-                        unique_tails.add(tuple(tail))
-            case "R":
-                while move[1]:
-                    move[1] -= 1
-                    head[0] += 1
-                    if abs(head[0] - tail[0]) > 1:
-                        tail[0] += 1
-                        tail[1] = head[1]
-                        unique_tails.add(tuple(tail))
-            case "L":
-                while move[1]:
-                    move[1] -= 1
-                    head[0] -= 1
-                    if abs(head[0] - tail[0]) > 1:
-                        tail[0] -= 1
-                        tail[1] = head[1]
-                        unique_tails.add(tuple(tail))
-    print(len(unique_tails))
+def draw(nodes):
+    """
+    Draws the rope in a grid_size x grid_size grid, 
+    highest number shown if nodes are on top of eachother
+    """
+    grid_size = 50
+    row = ['.'] * grid_size
+    grid = [deepcopy(row) for i in range(grid_size)]
+    node_num = 0
+    origin = (int(grid_size / 2) - 1)
+    grid[origin][origin] = "s"
+
+    for node in nodes:
+        x = node[0]
+        y = node[1]
+        x = x + (int(grid_size / 2) - 1)
+        y = -y + (int(grid_size / 2) - 1)
+        grid[y][x] = str(node_num)
+        node_num += 1
+
+    print("-" * (grid_size + 5))
+    row_num = 1
+    for row in grid:
+        print(f"{row_num:2}", end="")
+        row_num += 1
+        for char in row:
+            print(char, end="")
+        print(end='\n')
+    print("-" * (grid_size + 5))
 
 
-def move_node(head, tail, direction):
+def distance(node0, node1):
+    [x0, y0] = node0
+    [x1, y1] = node1
+    return sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2)
+
+
+def move_other_nodes(nodes):
+    """ Moves the other nodes according to the movement of head """
+    for i in range(len(nodes) - 1):
+        head = nodes[i]
+        tail = nodes[i + 1]
+        [x0, y0] = head
+        [x1, y1] = tail
+        # actual distance limit is sqrt(2) but next step would be sqrt(5) so using 2 is fine.
+        if distance(head, tail) >= 2:
+            # Head and tail are too far away, find out which way and move accordingly
+            if x0 == x1:  # On the same row, which means 2 steps Up or Down
+                if y0 > y1:
+                    y1 += 1
+                else:
+                    y1 -= 1
+            elif y0 == y1:  # On the same columns, which means 2 steps Left or Right
+                if x0 > x1:
+                    x1 += 1
+                else:
+                    x1 -= 1
+            else:  # Not touching, must move diagonally
+                if y0 > y1:
+                    y1 += 1
+                else:
+                    y1 -= 1
+                if x0 > x1:
+                    x1 += 1
+                else:
+                    x1 -= 1
+            tail[0] = x1
+            tail[1] = y1
+
+
+def move(nodes, direction):
+    """ Moves the head according to direction and then moves other nodes accordingly"""
+    head = nodes[0]
     match direction:
         case "U":
             head[1] += 1
-            if abs(head[1] - tail[1]) > 1:
-                tail[1] += 1
-                tail[0] = head[0]
         case "D":
             head[1] -= 1
-            if abs(head[1] - tail[1]) > 1:
-                tail[1] -= 1
-                tail[0] = head[0]
-        case "R":
-            head[0] += 1
-            if abs(head[0] - tail[0]) > 1:
-                tail[0] += 1
-                tail[1] = head[1]
         case "L":
             head[0] -= 1
-            if abs(head[0] - tail[0]) > 1:
-                tail[0] -= 1
-                tail[1] = head[1]
-    return (head, tail)
+        case "R":
+            head[0] += 1
+    move_other_nodes(nodes)
 
 
-def follow_node(head, tail):
-    diff_x = head[0] - tail[0]
-    diff_y = head[1] - tail[1]
-    if abs(diff_x) > 1:
-        tail[0] += 1 if diff_x > 0 else -1
-        tail[1] = head[1]
-
-    if abs(diff_y) > 1:
-        tail[1] += 1 if diff_y > 0 else -1
-        tail[0] = head[0]
-
-    return (head, tail)
-
-
-def second_task(moves):
-    nodes = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
-             [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+def first_task(lines):
+    nodes = [[0, 0], [0, 0]]
     unique_tails = set()
-    for move in moves:
-        for _ in range(move[1], 0, -1):
-            (nodes[0], nodes[1]) = move_node(nodes[0],
-                                                     nodes[1], move[0])
-            for i in range(1, len(nodes) - 1):
-                (nodes[i], nodes[i + 1]) = follow_node(nodes[i],
-                                                     nodes[i + 1])
+    for line in lines:
+        direction = line[0]
+        times = int(line[2:])
+        while times:
+            times -= 1
+            move(nodes, direction)
             unique_tails.add(tuple(nodes[-1]))
-    print(len(unique_tails))
+    print(f"first_task: {len(unique_tails)}")
+
+
+def second_task(lines):
+    nodes = [[0, 0] for _ in range(10)]
+    unique_tails = set()
+    for line in lines:
+        direction = line[0]
+        times = int(line[2:])
+        while times:
+            times -= 1
+            move(nodes, direction)
+            unique_tails.add(tuple(nodes[-1]))
+    print(f"second_task: {len(unique_tails)}")
 
 
 def main():
-    moves = []
-    with open("input.txt", "r") as input_file:
-        lines = input_file.readlines()
-        for line in lines:
-            moves.append([line[0], int(line[2:-1])])
-    # first_task(moves)
-    second_task(moves)
-    # 3432 is too high
-    # 2362 is too low
+    f = open("input.txt", "r")
+    lines = f.readlines()
+    first_task(lines)
+    second_task(lines)
 
 
 if __name__ == "__main__":
